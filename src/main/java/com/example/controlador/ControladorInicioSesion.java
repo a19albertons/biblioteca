341d6d4e2526d33b98fb17671cef7eeec3c52e71
a19albertons <a19albertons@iessanclemente.net>
2025-12-23 11:53:38 +0100

    Refactor InicioSesion

--- src/main/java/com/example/controlador/ControladorInicioSesion.java
+++ src/main/java/com/example/controlador/ControladorInicioSesion.java
@@ -1,6 +1,7 @@
 package com.example.controlador;
 
 import com.example.dao.UsuarioDAO;
+import com.example.modelo.TipoUsuario;
 import com.example.modelo.Usuario;
 
 /**
@@ -18,5 +19,39 @@ public class ControladorInicioSesion {
     public Usuario iniciarSesion(String usuario, String contrasena) {
         UsuarioDAO usuarioDAO = new UsuarioDAO();
         return usuarioDAO.consultaInicioSesion(usuario, contrasena);
+    }
+
+    /**
+     * Verifica si el usuario es no válido
+     * 
+     * @param usuario
+     */
+    public boolean usuarioNoValido(Usuario usuario) {
+        return usuario == null;
+    }
+
+    /**
+     * Verifica si la cuenta del usuario está desactivada
+     * 
+     * @param usuario
+     */
+    public boolean cuentaDesactivada(Usuario usuario) {
+        if (usuario == null) {
+            throw new IllegalArgumentException("Compruebe el usuario con usuarioNoValido antes de llamar a este método.");
+        }
+        return !usuario.getEstado();
+    }
+
+    /**
+     * Verifica si el usuario no es conserje
+     * 
+     * @param usuario
+     * @return
+     */
+    public boolean usuarioNoConserje(Usuario usuario) {
+        if (usuario == null) {
+            throw new IllegalArgumentException("Compruebe el usuario con usuarioNoValido antes de llamar a este método.");
+        }
+        return usuario.getTipo() != TipoUsuario.C;
     }
 }
