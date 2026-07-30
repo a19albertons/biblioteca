464d7419d9becb30841353dd653661736a3721b6
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-07-30 16:56:03 +0200

    Centralizar recursos de tipo imagen en codigo

---
+++ src/main/java/com/example/utilities/AppResources.java
@@ -0,0 +1,41 @@
+package com.example.utilities;
+
+import java.net.URL;
+
+/**
+ * Clase que contiene las rutas de los recursos de la aplicación
+ * AppResources
+ */
+public class AppResources {
+    public static final String LOGO_PATH = "/imagenes/logo.png";
+    public static final String EDITAR_PATH = "/imagenes/editar.png";
+    public static final String ELIMINAR_PATH = "/imagenes/eliminar.png";
+
+    private AppResources() {
+        // Evitar instanciación
+    }
+
+    /**
+     * Devuelve la URL del logo de la aplicación
+     * @return URL del logo de la aplicación
+     */
+    public static URL logoPath() {
+        return AppResources.class.getResource(LOGO_PATH);
+    }
+
+    /**
+     * Devuelve la URL del icono de editar
+     * @return URL del icono de editar
+     */
+    public static URL editarPath() {
+        return AppResources.class.getResource(EDITAR_PATH);
+    }
+
+    /**
+     * Devuelve la URL del icono de eliminar
+     * @return URL del icono de eliminar
+     */
+    public static URL eliminarPath() {
+        return AppResources.class.getResource(ELIMINAR_PATH);
+    }
+}
